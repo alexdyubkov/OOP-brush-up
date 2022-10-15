b4e2732393deaafb38e3e6254009eae706768d38
Alex <a.dziubkou@gmail.com>
2022-10-15 14:43:34 +0300

    Update OOP brush up.py

--- OOP brush up.py
+++ OOP brush up.py
@@ -614,5 +614,146 @@ laptop = Laptop('Acer', 'Predator', 5490)
 print(laptop.__dict__)
        
 
+#10.1 Implement a class called Laptop that sets the following instance attributes when creating an instance:
+#brand as a bare instance attribute
+#model as a protected attribute
+#price as a private attribute
+#Then create an instance named laptop with the following arguments:
+# 'Acer'
+# 'Predator'
+# 5490
+# In response, print the value of the __dict__ attribute of the laptop instance.
+
+#Results
+# {'brand': 'Acer', '_model': 'Predator', '_Laptop__price': 5490}
+
+class Laptop:
+    def __init__(self,brand, model, price):
+        self.brand=brand
+        self._model=model
+        self.__price=price
+
+laptop=Laptop(brand='Acer', model='Predator', price=5490)
+
+print(laptop.__dict__)
+
+
+#10.2A class called Laptop was implemented. Then, an instance of the Laptop class named laptop was created with the following arguments:
+# 'Acer'
+# 'Predator'
+# 5490
+# In response, print the value for each instance attribute (on a separate line) of the laptop instance as shown below.
+# brand -> Acer
+# model -> Predator
+# price -> 5490
+
+class Laptop:
+    def __init__(self,brand, model, price):
+        self.brand=brand
+        self._model=model
+        self.__price=price
+
+laptop=Laptop(brand='Acer', model='Predator', price=5490)
+
+# print(laptop.__dict__)
+
+res=[]
+for i,j in laptop.__dict__.items():
+    res.append(str(i)+' -> '+str(j))
+
+for i in res:
+    i=i.replace('_','').replace('Laptop','')
+    print(i)
+
+
+#or
+class Laptop:
+
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self._model = model
+        self.__price = price
+
+
+laptop = Laptop('Acer', 'Predator', 5490)
+print(f'brand -> {laptop.brand}')
+print(f'model -> {laptop._model}')
+print(f'price -> {laptop._Laptop__price}')
+
+
+
+
+
+
+#10.3An implementation of the Laptop class is given. Implement a method in the Laptop class called display_private_attrs() that displays the names of all private attributes of the instance. Then create an instance with the given arguments:
+# 'Acer'
+# 'Predator'
+# 'AC-100'
+# 5490
+# 0.2
+# and assign it to the variable laptop. In response, call display_private_attrs() on the laptop instance.
+
+# Expected result:
+# _Laptop__price
+# _Laptop__margin
+
+
+class Laptop:
+
+    def __init__(self, brand, model, code, price, margin):
+        self.brand = brand
+        self._model = model
+        self._code = code
+        self.__price = price
+        self.__margin = margin
+    
+    def display_private_attrs(self):
+        for i in laptop.__dict__.keys():
+            if '__' in i:
+                print(i)
+
+laptop = Laptop('Acer','Predator','AC-100', 5400, 0.2)
+laptop.display_private_attrs()
+
+
+
+
+
+
+
+
+#10.4An implementation of the Laptop class is given. Implement a method called display_protected_attrs() in the Laptop class that displays the names of the protected attribute of the instance. Then create an instance with the given arguments:
+# 'Acer'
+# 'Predator'
+# 'AC-100'
+# 5490
+# 0.2
+# and assign it to the variable laptop. In response, call display_protected_attrs() on the laptop instance.
+
+# Expected result:
+# _model
+# _code
+
+class Laptop:
+
+    def __init__(self, brand, model, code, price, margin):
+        self.brand = brand
+        self._model = model
+        self._code = code
+        self.__price = price
+        self.__margin = margin
+    
+    def display_private_attrs(self):
+        for i in laptop.__dict__.keys():
+            if '_'  in i and not  '__' in i:
+                print(i)
+
+
+laptop = Laptop('Acer','Predator','AC-100', 5400, 0.2)
+
+laptop.display_private_attrs()
+
+
+
 
 
