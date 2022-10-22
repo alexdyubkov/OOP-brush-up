530365270ce79e489ec9ffbed94115cbf27c5ebd
Alex <a.dziubkou@gmail.com>
2022-10-23 00:03:55 +0300

    Update OOP brush up.py

--- OOP brush up.py
+++ OOP brush up.py
@@ -756,4 +756,65 @@ laptop.display_private_attrs()
 
 
 
+#12.1 Implement a class called Laptop which in the __init __() method sets the value of the price protected attribute that stores the price of the laptop (without any validation). Then implement a method to read that attribute named get_price() and a method to modify that attribute named set_price() without validation as well.
+#Then create an instance of the Laptop class with a price of 3499 and follow these steps:
+#using the get_price() method print the value of the price protected attribute to the console
+#using the set_price() method, set the value of the price protected attribute to 3999
+#using the get_price() method print the value of the price protected attribute to the console
+
+#Expected result:
+#3499
+#3999
+
+class Laptop:
+    def __init__(self, price):
+        self._price = price
+    def get_price(self):
+        return self._price
+    def set_price(self, value):
+        self._price = value
+laptop = Laptop(3499)
+print(laptop.get_price())
+laptop.set_price(3999)
+print(laptop.get_price())
+
+
+
+
+#12.2 A class called Laptop was implemented. Implement a method named set_price() to modify price attribute that validates the value. Validation checks:
+# whether the value is an int or float type, if it is not raise a TypeError with the following message:
+# 'The price attribute must be an int or float type.'
+# whether the value is positive, if it is not raise ValueError with the following message:
+# 'The price attribute must be a positive int or float value.'
+# Then create an instance of the Laptop class with a price of 3499 and try to set '-3000' to the price using set_price() method. If an error is raised, print the error message to the console. Use a try ... except ... clause in your solution.
+
+
+# Expected result:
+# The price attribute must be an int or float type.
+
+class Laptop:
+    def __init__(self, price):
+        self._price = price
+    def get_price(self):
+        return self._price
+    def set_price(self, value):
+        if isinstance(value, int or float):
+            if value > 0:
+                self._price = value
+            else:
+                raise ValueError('The price attribute must be a positive int or float value.')
+        else:
+            raise TypeError('The price attribute must be an int or float type.')
+
+laptop = Laptop(3499)
+
+try:
+    laptop.set_price('-3000')
+except TypeError as error:
+    print(error)
+
+
+
+
+
 
